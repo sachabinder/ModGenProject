dba9074d3356f007836990300eb28dd2f7d31841
sachabinderenpc <sacha.binder@eleves.enpc.fr>
2025-03-23 01:10:54 +0000

    other gray rip

--- runners/colored_mask_results.sh
+++ runners/colored_mask_results.sh
@@ -24,68 +24,68 @@ python main.py \
   --impating_mask_color gray
 
 
-python main.py \
-  --data ./test_image \
-  --out ./paper_experiment/rip_colors/blue/dps \
-  --scale 4.8 \
-  --algo dps \
-  --operator rip \
-  --nstep 500 \
-  --model stabilityai/stable-diffusion-2-base \
-  --rand_impating_proportion 0.25 \
-  --impating_mask_color blue
+# python main.py \
+#   --data ./test_image \
+#   --out ./paper_experiment/rip_colors/blue/dps \
+#   --scale 4.8 \
+#   --algo dps \
+#   --operator rip \
+#   --nstep 500 \
+#   --model stabilityai/stable-diffusion-2-base \
+#   --rand_impating_proportion 0.25 \
+#   --impating_mask_color blue
 
-python main.py \
-  --data ./test_image \
-  --out ./paper_experiment/rip_colors/blue/psld \
-  --scale 3.5 \
-  --algo psld \
-  --operator rip \
-  --nstep 500 \
-  --model stabilityai/stable-diffusion-2-base \
-  --rand_impating_proportion 0.25 \
-  --impating_mask_color blue
+# python main.py \
+#   --data ./test_image \
+#   --out ./paper_experiment/rip_colors/blue/psld \
+#   --scale 3.5 \
+#   --algo psld \
+#   --operator rip \
+#   --nstep 500 \
+#   --model stabilityai/stable-diffusion-2-base \
+#   --rand_impating_proportion 0.25 \
+#   --impating_mask_color blue
 
-  python main.py \
-  --data ./test_image \
-  --out ./paper_experiment/rip_colors/red/dps \
-  --scale 4.8 \
-  --algo dps \
-  --operator rip \
-  --nstep 500 \
-  --model stabilityai/stable-diffusion-2-base \
-  --rand_impating_proportion 0.25 \
-  --impating_mask_color red
+#   python main.py \
+#   --data ./test_image \
+#   --out ./paper_experiment/rip_colors/red/dps \
+#   --scale 4.8 \
+#   --algo dps \
+#   --operator rip \
+#   --nstep 500 \
+#   --model stabilityai/stable-diffusion-2-base \
+#   --rand_impating_proportion 0.25 \
+#   --impating_mask_color red
 
-python main.py \
-  --data ./test_image \
-  --out ./paper_experiment/rip_colors/red/psld \
-  --scale 3.5 \
-  --algo psld \
-  --operator rip \
-  --nstep 500 \
-  --model stabilityai/stable-diffusion-2-base \
-  --rand_impating_proportion 0.25 \
-  --impating_mask_color red
+# python main.py \
+#   --data ./test_image \
+#   --out ./paper_experiment/rip_colors/red/psld \
+#   --scale 3.5 \
+#   --algo psld \
+#   --operator rip \
+#   --nstep 500 \
+#   --model stabilityai/stable-diffusion-2-base \
+#   --rand_impating_proportion 0.25 \
+#   --impating_mask_color red
 
-    python main.py \
-  --data ./test_image \
-  --out ./paper_experiment/rip_colors/green/dps \
-  --scale 4.8 \
-  --algo dps \
-  --operator rip \
-  --nstep 500 \
-  --model stabilityai/stable-diffusion-2-base \
-  --rand_impating_proportion 0.25 \
-  --impating_mask_color green
+#   python main.py \
+#   --data ./test_image \
+#   --out ./paper_experiment/rip_colors/green/dps \
+#   --scale 4.8 \
+#   --algo dps \
+#   --operator rip \
+#   --nstep 500 \
+#   --model stabilityai/stable-diffusion-2-base \
+#   --rand_impating_proportion 0.25 \
+#   --impating_mask_color green
 
-python main.py \
-  --data ./test_image \
-  --out ./paper_experiment/rip_colors/green/psld \
-  --scale 3.5 \
-  --algo psld \
-  --operator rip \
-  --nstep 500 \
-  --model stabilityai/stable-diffusion-2-base \
-  --rand_impating_proportion 0.25 \
-  --impating_mask_color green
+# python main.py \
+#   --data ./test_image \
+#   --out ./paper_experiment/rip_colors/green/psld \
+#   --scale 3.5 \
+#   --algo psld \
+#   --operator rip \
+#   --nstep 500 \
+#   --model stabilityai/stable-diffusion-2-base \
+#   --rand_impating_proportion 0.25 \
+#   --impating_mask_color green
